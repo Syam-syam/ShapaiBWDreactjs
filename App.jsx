70d7888833bcbd619e49e13aa7452812f1e17692
Syam-syam <85424673+Syam-syam@users.noreply.github.com>
2021-06-06 11:34:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha we covered
+        everything from scratch including JavaScript,React.js,HTML .
+      </p>
+    </div>
+  );
+}
+export default Note;
